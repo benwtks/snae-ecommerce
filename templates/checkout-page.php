55a8279b1dcfbddcba1068104f440732da706281
Ben Watkins <contact@benwtks.com>
2020-07-16 00:28:31 +0100

    Add more errors

--- templates/checkout-page.php
+++ templates/checkout-page.php
@@ -4,8 +4,9 @@ $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
 $workshops = snae_ecommerce_workshop_ids($_POST['cart_items']);
 $secret_key = carbon_get_theme_option('crb_stripe_api_key_secret');
+$publishable_key = carbon_get_theme_option('crb_stripe_api_key_publishable');
 
-if (!$_POST['cart_items'] || !$secret_key) {
+if (!$_POST['cart_items'] || !$secret_key || !$publishable_key) {
 	wp_redirect(snae_ecommerce_get_cart_url());
 	exit();
 }
@@ -14,6 +15,22 @@ try {
 	$intent = snae_ecommerce_create_intent($secret_key, $workshops);
 } catch (\Stripe\Exception\InvalidArgumentException | \Stripe\Exception\ApiErrorException | \Stripe\Exception\ApiErrorException $e) {
 	wp_redirect(snae_ecommerce_get_cart_url() . '?error=' . $e->getMessage());
+	exit();
+}
+
+
+if(!$intent) {
+	wp_redirect(snae_ecommerce_get_cart_url() . '?error=could-not-create-intent');
+	exit();
+}
+
+if (!snae_ecommerce_get_cart_url()) {
+	wp_redirect(snae_ecommerce_get_cart_url() . '?error=error-getting-cart');
+	exit();
+}
+
+if (!carbon_get_theme_option('crb_success_page')) {
+	wp_redirect(snae_ecommerce_get_cart_url() . '?error=error-getting-success-page');
 	exit();
 }
 
